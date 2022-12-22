fe4ee0b68dabb08ef56bf24fd9cac38346d961ff
Wojciech Domalewski <info@pnti.pl>
2022-12-22 09:30:24 +0100

    Dodanie loadera js

--- wasm_loader.js
+++ wasm_loader.js
@@ -9,5 +9,17 @@ const w = 150, height= 150;
     const exports = instance.exports;
     const buffer = exports.mem.buffer;
     const canvasData = new Uint8Array(buffer, 0x10000, w*h*4);
-})
 
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2D');
+    const imageData = context.createImageData(w, h);
+
+(function update(){
+    requestAnimationFrame(update);
+    exports.run();
+
+    imageData.data.set(canvasData);
+    context.putImageData(imageData, 0, 0);
+    
+})();
+})()
